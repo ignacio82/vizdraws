1e4901f39706c37c8ec0d1968cbc3ce49c4bb53b
Balaj Khan <balajkhan07@gmail.com>
2022-02-05 00:21:30 +0500

    download svg icon added

--- inst/htmlwidgets/lib/vizdraws/vizdraws-general.js
+++ inst/htmlwidgets/lib/vizdraws/vizdraws-general.js
@@ -237,14 +237,15 @@ function factory(el, width, height) {
             return (first + rest);
           };
       
-        //define a button to download chart as png
+      //define a button to download chart as png
+      const ICON = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-download'><path d='M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4'></path><polyline points='7 10 12 15 17 10'></polyline><line x1='12' y1='15' x2='12' y2='3'></line></svg>";
       let save_as_image = svg.append('g')
-      .append('image')
-      .attr('xlink:href', 'https://www.nvcc.edu/honors/_img/download.png')
-      .attr('width', 20)
-      .attr('height', 20)
-      .attr('x', 50)
-      .attr('y', 0)
+        .append('svg')
+        .attr('width', 20)
+        .attr('height', 20)
+        .attr('x', 50)
+        .attr('y', 0)
+        .html(ICON);
 
     let triggerDownload = (imgURI) => {
       const evt = new MouseEvent('click', {
